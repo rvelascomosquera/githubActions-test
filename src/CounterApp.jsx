a2f41440ce8b58e6a9fa83bf99a7d2c76202b2a1
robertvelasco <robert.velasco.cel@gmail.com>
2022-08-22 17:36:46 -0500

    instalacion de eslint y correcion de errores

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,25 +1,22 @@
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
+import { useState } from 'react'
+import PropTypes from 'prop-types'
 
-export const CounterApp = ({value}) => {
-
-  const [counter, setCounter] = useState (value)
-
-  const handleAdd = () => { setCounter (counter + 1) }
-  const handleSubtract = () => { setCounter (counter - 1) }
-  const handleReset = () => { setCounter (value) }
+// eslint-disable-next-line react/prop-types
+export function CounterApp ({ value }) {
+  const [counter, setCounter] = useState(value)
+  const handleAdd = () => { setCounter(counter + 1) }
+  const handleSubtract = () => { setCounter(counter - 1) }
+  const handleReset = () => { setCounter(value) }
 
   return (
     <>
       <h1>Mi Contador</h1>
-      <h2>{ counter }</h2>
-      <button onClick={ handleAdd }>+</button>
-      <button onClick={ handleSubtract }>-</button>
-      <button aria-label='reset' onClick={ handleReset }>Reset</button>
+      <h2>{counter}</h2>
+      <button onClick={handleAdd}>+</button>
+      <button onClick={handleSubtract}>-</button>
+      <button aria-label='reset' onClick={handleReset}>Reset</button>
     </>
   )
 }
 
-CounterApp.prototypes = {
-  value: PropTypes.number
-}
+CounterApp.prototypes = { value: PropTypes.number }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CounterApp } from './CounterApp';
-import './styles.css';
+import { CounterApp } from './CounterApp'
+import './styles.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render (
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
     <CounterApp value={10} />
   </React.StrictMode>
 )
